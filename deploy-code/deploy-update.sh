b71a5ed90696a1f9b10e2666e2e3676a88bfd416
Viggo <104303404+viggoStrom@users.noreply.github.com>
2025-01-27 16:15:49 +0100

    Added force checkouts to deploy script

--- deploy-code/deploy-update.sh
+++ deploy-code/deploy-update.sh
@@ -7,11 +7,11 @@
 # If the branch or tag is not provided, default to main
 git fetch --all
 if [ "$1" == "tag" ]; then
-    git checkout tags/$2
+    git checkout -f tags/$2
 elif [ "$1" == "branch" ]; then
-    git checkout $2
+    git checkout -f $2
 else
-    git checkout origin/main
+    git checkout -f origin/main
     # Inform user that the branch or tag is not provided
     echo "Branch or tag is not provided. Defaulting to origin/main branch."
 fi
