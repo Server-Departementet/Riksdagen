35e4abc5866a195251a8baf7e0703682782ca5c4
viggoStrom <viggostrom05@gmail.com>
2025-01-28 11:11:30 +0100

    removed concurrently and reworked package scripts

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,34 @@
 require("./discord/discord-bot.ts");
+
+
+// DB basics
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const write = async (name: string) => {
+    const user = await prisma.user.create({
+        data: {
+            name: name,
+        }
+    });
+
+    console.log("Added user:", user);
+};
+
+const read = async () => {
+    const user = await prisma.user.findMany();
+    console.log(user);
+};
+
+write("John Doe")
+    .catch(e => { throw e })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
+
+read()
+    .catch(e => { throw e })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
