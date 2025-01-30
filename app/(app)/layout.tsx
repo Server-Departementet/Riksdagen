6ef6c2e9b0876dafb08149f2889afcac2af22eb6
viggoStrom <viggostrom05@gmail.com>
2025-01-30 08:12:58 +0100

    rewrote how discord OAuth links are defined

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,26 +2,24 @@ import "./global.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-let discordOAuthLink: string;
-if (process.env.NODE_ENV === "development") {
-    discordOAuthLink = "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Fdiscord&scope=identify";
-}
-else {
-    discordOAuthLink = "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=https%3A%2F%2Fserver-riksdagen.tailad6f63.ts.net%2Foauth%2Fdiscord&scope=identify";
-}
+// Discord OAuth link
+const discordOAuthLink = process.env.NODE_ENV === "development" ?
+    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Fdiscord&scope=identify" :
+    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=https%3A%2F%2Fserver-riksdagen.tailad6f63.ts.net%2Foauth%2Fdiscord&scope=identify";
 
 export default async function AppLayout({ children }: {
     children: React.ReactNode
 }) {
     return (<>
+        {/* Header */}
         <header className="p-3 px-5 items-center">
             {/* Logo */}
-            <div className="flex flex-row items-center gap-x-4">
-                <Link href="/">
+            <div className="flex flex-row">
+                <Link href="/" className="flex flex-row items-center gap-x-4 no-underline">
                     <Image width={64} height={64} className="size-[3.5rem] rounded-lg" src="/icons/header-logo.png" alt="Logo" />
-                </Link>
 
-                <p className="text-2xl font-medium">Regeringen</p>
+                    <p className="text-2xl font-medium">Riksdagen</p>
+                </Link>
             </div>
 
             {/* Discord Login */}
