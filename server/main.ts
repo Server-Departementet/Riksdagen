fd980612bc9e958c0161bfc9eafbaf377348ce72
viggoStrom <viggostrom05@gmail.com>
2025-01-31 10:22:02 +0100

    save discord user info to db

--- server/main.ts
+++ server/main.ts
@@ -3,41 +3,9 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 /// <reference path="extend/console.ts" />
 /// <reference path="discord/discord-bot.ts" />
 /// <reference path="oauth/discord-oauth.ts" />
-
-// // DB basics
-// const { PrismaClient } = require("@prisma/client");
-
-// const prisma = new PrismaClient();
-
-// const write = async () => {
-//     const user = await prisma.user.create({
-//         data: {
-//             username: "John Doe" + Date.now(),
-//             nickname: "John",
-//             avatar: "url",
-//             refreshToken: "COol token",
-//         }
-//     });
-
-//     console.log("Added user:", user);
-// };
-
-// const read = async () => {
-//     const user = await prisma.user.findMany();
-//     console.log(user);
-// };
-
-// write()
-//     .catch(e => { throw e })
-//     .finally(async () => {
-//         await prisma.$disconnect();
-//     });
-
-// read()
-//     .catch(e => { throw e })
-//     .finally(async () => {
-//         await prisma.$disconnect();
-//     });
--- server/oauth/discord-oauth.ts
+++ server/oauth/discord-oauth.ts
@@ -1,22 +1,40 @@
 const express = require("express");
 
+// Express setup
 const app = express();
 const port = 4000;
-
+let lastRequest = 0;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((_: any, res: any, next: any) => {
+app.use((_: any, res: any, next: any) => { // CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// Where Discord sends the user after they authorize the app. Matches with discordOAuthLink in (app)/layout.tsx
+const redirectURI = process.env.ENV === "server" ?
+    "https://server-riksdagen.tailad6f63.ts.net/oauth/discord"
+    :
+    "http://localhost:3000/oauth/discord"
+    ;
+
+// Endpoint for receiving Discord access and refresh tokens
 app.post("/api/oauth/discord", async (req: any, res: any) => {
-    console.info("Got request");
+
+    // Debounce time on requests
+    if (Date.now() - lastRequest < 10) {
+        return res.status(429).json({ error: "Too many requests" });
+    }
+    lastRequest = Date.now();
+
+    console.info("Got request to /api/oauth/discord");
 
     const body: { code: string } = req.body;
 
+    // Request validation
     if (!body || !body.code) {
+        console.warn("Invalid request from client", body);
         return res.status(400).json({ error: "Invalid request" });
     }
 
@@ -26,15 +44,11 @@ app.post("/api/oauth/discord", async (req: any, res: any) => {
         ||
         !process.env.DISCORD_CLIENT_SECRET
     ) {
+        console.error("Missing environment variables: DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET");
         return res.status(500).json({ error: "Internal server error" });
     }
 
-    const redirectURI = process.env.ENV === "server" ?
-        "https://server-riksdagen.tailad6f63.ts.net/oauth/discord"
-        :
-        "http://localhost:3000/oauth/discord"
-        ;
-
+    // Requesting access token from Discord
     const tokenResponseData = await fetch(
         "https://discord.com/api/oauth2/token",
         {
@@ -54,12 +68,59 @@ app.post("/api/oauth/discord", async (req: any, res: any) => {
     );
 
     if (!tokenResponseData.ok) {
+        console.error("Token response not ok", tokenResponseData);
         return res.status(500).json({ error: "Internal server error" });
     }
 
     const tokenData = await tokenResponseData.json();
 
-    console.log(tokenData);
+    console.log("Token", tokenData); // TODO - remove
+
+    // Get identity of the user
+    const identityResponseData = await fetch(
+        "https://discord.com/api/v10/users/@me",
+        {
+            headers: {
+                "Authorization": `${tokenData.token_type} ${tokenData.access_token}`,
+            },
+        }
+    );
+
+    if (!identityResponseData.ok) {
+        console.error("Identity response not ok", identityResponseData);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+
+    const identityData = await identityResponseData.json();
+
+    console.log("Identity", identityData); // TODO - remove
+
+    const user = {
+        id: identityData.id,
+        username: identityData.username,
+        displayName: identityData.global_name,
+        avatar: `https://cdn.discordapp.com/avatars/${identityData.id}/${identityData.avatar}.png`,
+        discordRefreshToken: tokenData.refresh_token,
+    }
+
+    // Add or update user in DB
+    await prisma.user.create({ data: user })
+        .catch((error: any) => {
+            // Existing user in DB
+            if (error.code === "P2002") {
+                console.info("Updated existing user", user);
+                return prisma.user.update({
+                    where: { id: user.id },
+                    data: user,
+                });
+            }
+
+            console.error("Error creating user", error);
+            return res.status(500).json({ error: "Internal server error" });
+        })
+        .finally(() => {
+            prisma.$disconnect();
+        });
 
     return res.status(200).json({ code: body.code });
 });
