828b2125ad8cee1524cd113b9e80dd6153ab16ac
viggoStrom <viggostrom05@gmail.com>
2025-02-01 11:15:11 +0100

    fixed discord oauth routing

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,9 +5,9 @@ import { Suspense } from "react";
 
 // Discord OAuth link
 const discordOAuthLink = process.env.ENV === "server" ?
-    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=https%3A%2F%2Fserver-riksdagen.tailad6f63.ts.net%2Foauth%2Fdiscord&scope=identify"
+    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=https%3A%2F%2Fserver-riksdagen.tailad6f63.ts.net%2Fapi%2Foauth%2Fdiscord&scope=identify"
     :
-    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Fdiscord&scope=identify"
+    "https://discord.com/oauth2/authorize?client_id=1222824481571999856&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Foauth%2Fdiscord&scope=identify"
     ;
 
 export default async function AppLayout({ children }: {
