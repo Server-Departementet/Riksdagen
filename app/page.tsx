d97ac933c26324f60d16b5a998ad2bdc440471f4
viggoStrom <viggostrom05@gmail.com>
2025-02-03 07:54:56 +0100

    removed dead code

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import ministerWall from "./(components)/minister-wall";
-import { useEffect } from "react";
 
 export default function Page() {
-    const searchParams = useSearchParams();
-
-    useEffect(() => {
-        // TODO - Make this way better. Use avatar and username from Discord to show that the user is logged in
-        if (searchParams?.get("oauth") === "success") {
-            const discordLoginButton = document.getElementById("discord-login-button-in-header");
-
-            if (discordLoginButton) {
-                discordLoginButton.innerHTML = discordLoginButton.innerHTML.replace("Login", "Logged in") || "Login";
-            }
-        }
-    });
-
     return (
         <main>
             <h1 className="mt-14">Välkommen till Riksdagen</h1>
