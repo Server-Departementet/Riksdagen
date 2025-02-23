4a4d24673af127198f50464f46c32b7a75fffff6
viggoStrom <viggostrom05@gmail.com>
2025-02-23 20:33:55 +0100

    add markdown parser

--- app/components/minister-wall.tsx
+++ app/components/minister-wall.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import ministersDB from "@root/db/ministers.json";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion";
+import md from "@/lib/markdown";
 
 const ministerAccordionItems = Object.entries(ministersDB).map(([ministerID, minister]) => {
     return (
         <AccordionItem key={ministerID} value={ministerID}>
-            <AccordionTrigger>{minister.title}</AccordionTrigger>
+            <AccordionTrigger>
+                <span dangerouslySetInnerHTML={md(minister.title)}></span>
+            </AccordionTrigger>
             <AccordionContent>
-                <h4 className="mb-1">{minister.name}</h4>
-                <p>{minister.description} {" "}
+                <h4 className="mb-1" dangerouslySetInnerHTML={md(minister.name)}></h4>
+                <p>
+                    <span dangerouslySetInnerHTML={md(minister.description)}></span>
+                    {" "}
                     <Link href={`/ministrar/${ministerID}`}>Läs mer</Link>
                     .
                 </p>
@@ -23,7 +28,7 @@ const ministerWall = (
             <h2 className="w-full text-center mb-1">Ministerposterna</h2>
         </Link>
 
-        <Accordion className="w-10/12 max-w-prose" type="single" collapsible>
+        <Accordion className="w-10/12 max-w-prose" type="multiple">
             {ministerAccordionItems}
         </Accordion>
     </section>
---
+++ app/lib/markdown.ts
@@ -0,0 +1,13 @@
+import { marked } from "marked";
+
+/**
+ * Convert markdown strings to HTML objects to be used with React's dangerouslySetInnerHTML
+ * @param text - The markdown string to be converted
+ * @returns - { __html: string } object to be used with React's dangerouslySetInnerHTML
+ */
+const md = (text: string): TrustedHTML => {
+    console.log(marked.parseInline(text));
+    return { __html: marked.parseInline(text) };
+};
+
+export default md;
