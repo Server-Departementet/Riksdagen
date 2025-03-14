d37d7239fd7066711efee41dd25d2ee39b55ab19
viggoStrom <viggostrom05@gmail.com>
2025-03-14 08:46:55 +0100

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,31 @@
 import * as Icon from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Custom404() {
   return (
     <main className="pb-20 flex flex-col gap-y-2 justify-center items-center">
-      <h1 className="text-7xl">404</h1>
-      <p className="text-xl">Sidan hittades inte</p>
-      <Icon.Frown strokeWidth={1} size={32} />
 
-      <Link className="unset mt-10" href={"/"}>
-        <button className="button">Åk hem</button>
-      </Link>
+      <section className="z-10 flex flex-col gap-y-[inherit] justify-center items-center">
+        {/* Message */}
+        <h1 className="text-7xl">404</h1>
+        <p className="text-xl">Sidan hittades inte</p>
+        <Icon.Frown strokeWidth={1} size={32} />
+
+        {/* Go home button */}
+        <Link className="unset mt-10" href={"/"}>
+          <button className="button">Åk hem</button>
+        </Link>
+      </section>
+
+      {/* Big background crowns */}
+      <div className="absolute opacity-50 flex flex-col justify-center items-center gap-x-2">
+        <div className="flex flex-row justify-center items-center gap-x-2">
+          <Image src={"icons/crown.svg"} height={230} width={230} alt="" />
+          <Image src={"icons/crown.svg"} height={230} width={230} alt="" />
+        </div>
+        <Image src={"icons/crown.svg"} height={230} width={230} alt="" />
+      </div>
     </main>
   )
 }
