f2d7a0b54fdc0c054115ff45305298f0abbb11a0
viggoStrom <ViggoStrom05@gmail.com>
2025-03-22 20:31:13 +0100

    update server scripts

--- deploy-code/yarn-start.sh
+++ deploy-code/yarn-start.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# If NVM_BIN is set, use it directly
-if [ -n "$NVM_BIN" ]; then
-    cd /root/Riksdagen/
-    ${NVM_BIN}/yarn start
-else
-    # Fallback to loading NVM properly if NVM_BIN isn't set
-    export NVM_DIR="/root/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    
-    cd /root/Riksdagen/
-    yarn start
-fi
+cd /root/Riksdagen/
+${NVM_BIN}/yarn start
