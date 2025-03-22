74885cd65806f1b0d4c5c6b2132383581445b297
viggoStrom <ViggoStrom05@gmail.com>
2025-03-22 20:40:05 +0100

    update server scripts

--- deploy-code/yarn-start.sh
+++ deploy-code/yarn-start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Add Node.js bin directory to PATH
+export PATH="/root/.nvm/versions/node/v23.10.0/bin:$PATH"
+
 cd /root/Riksdagen/
 /root/.nvm/versions/node/v23.10.0/bin/yarn start
