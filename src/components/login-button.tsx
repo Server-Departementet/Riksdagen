1da07566535d70c3f022b0f24fb88e43b3e8a290
viggoStrom <ViggoStrom05@gmail.com>
2025-03-23 17:36:13 +0100

    make login button a button for accessibility

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -5,13 +5,13 @@ type NameSide = "left" | "right" | "none";
 
 function DiscordButton() {
   return (
-    <div
+    <button
       tabIndex={0}
       className="w-min flex flex-row items-center justify-center gap-x-2 px-7 py-2.5 bg-[#5865f2] text-white rounded-lg font-bold no-underline hover:text-white hover:drop-shadow-lg cursor-pointer select-none"
     >
       <Image width={24} height={24} src="/icons/discord/discord-white.svg" alt="Discord"></Image>
       Login
-    </div>
+    </button>
   );
 }
 
