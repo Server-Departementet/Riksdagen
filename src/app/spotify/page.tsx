8e630db8895225a003c60e2dd1839344a961517d
viggoStrom <viggostrom05@gmail.com>
2025-04-12 18:04:07 +0200

    update spotify page

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -26,7 +26,7 @@ async function getUserData(userId: string) {
         }
       },
     },
-    take: 3, // TODO: remove
+    take: 10, // TODO: remove
   });
 }
 
@@ -36,11 +36,70 @@ export default async function SpotifyPage() {
     .map(user => ({ name: user.firstName, id: user.id, data: getUserData(user.id) }))
     .reverse();
 
+  let totalMS: number = 0;
+  for (const user of users) {
+    const data = await user.data;
+    totalMS += data.map(play => play.track).reduce((acc, track) => acc + track.duration, 0); // ms
+  }
+
+  const awaitedUsers = await Promise.all(users.map(async user => {
+    const data = await user.data;
+    return { ...user, data };
+  }));
+
+  // {awaitedUsers
+  //   .flatMap(user => user.data)
+  //   .map(play => play.track)
+  //   .map(track => {
+  //     const trackMS = awaitedUsers
+  //       .flatMap(user => user.data)
+  //       .filter(play => play.track.id === track.id)
+  //       .map(play => play.track)
+  //       .reduce((acc, track) => acc + track.duration, 0); // ms
+  //     return [track.id, trackMS];
+  //   })
+  //   // Sort by listened time
+  //   .sort((a, b) => {
+  //     const aTime = a[1] || 0;
+  //     const bTime = b[1] || 0;
+  //     return parseFloat(bTime.toString()) - parseFloat(aTime.toString());
+  //   })
+  //   .map(([trackId, trackMS]) => {
+  //     const track = awaitedUsers.flatMap(user => user.data).find(play => play.track.id === trackId)?.track;
+  //     if (!track) return null;
+
+  //     return (
+  //       <TrackPlay track={track} listeningTime={parseFloat(trackMS.toString())} username={null} key={track.id + "-" + trackId} />
+  //     );
+  //   })}
+  const mostListenedTracks = awaitedUsers
+    .flatMap(user => user.data)
+    .map(play => play.track)
+    .map(track => {
+      const trackMS = awaitedUsers
+        .flatMap(user => user.data)
+        .filter(play => play.track.id === track.id)
+        .map(play => play.track)
+        .reduce((acc, track) => acc + track.duration, 0); // ms
+      return [track.id, trackMS];
+    })
+    // Remove duplicates
+    .filter((track, i, self) => self.findIndex(t => t[0] === track[0]) === i)
+    // Sort by listened time
+    .sort((a, b) => {
+      const aTime = a[1] || 0;
+      const bTime = b[1] || 0;
+      return parseFloat(bTime.toString()) - parseFloat(aTime.toString());
+    })
+    .slice(0, 3);
+
+
+
   return (
     <main>
       <h1 className="mt-10">Spotify Statistik</h1>
 
-      <Tabs className="mt-5 mb-10 w-full sm:w-10/12 flex flex-col items-center" defaultValue={(await headers()).get("x-opened-page") || "alla"}>
+      <Tabs className="mt-5 mb-10 w-full lg:w-10/12 flex flex-col items-center" defaultValue={(await headers()).get("x-opened-page") || "alla"}>
         {/* List */}
         <TabsList className="w-full mb-1 flex flex-row justify-start overflow-x-scroll">
           {/* All */}
@@ -61,12 +120,9 @@ export default async function SpotifyPage() {
         </TabsList>
 
         {/* Totals tab */}
-        <TabsContent tabIndex={-1} value="alla" className="w-full sm:w-8/12">
-          Total tid mellan alla användare:
-          {users.map(async (user, i) => {
-            const data = await user.data;
-
-            const totalMS = data.map(play => play.track).reduce((acc, track) => acc + track.duration, 0); // ms
+        <TabsContent tabIndex={-1} value="alla" className="w-full lg:w-8/12">
+          <h3>Totala tiden mellan alla</h3>
+          {(() => {
             const timeInDifferentUnits = {
               s: { time: totalMS / 1000, unitLong: "sekunder", unitShort: "s" },
               min: { time: totalMS / 60000, unitLong: "minuter", unitShort: "min" },
@@ -78,7 +134,7 @@ export default async function SpotifyPage() {
             };
 
             return (
-              <div key={user.id + "-" + i} className="flex flex-row gap-x-2 whitespace-nowrap overflow-x-scroll">
+              <div className="flex flex-row gap-x-2 justify-center whitespace-nowrap overflow-x-scroll">
                 <TooltipProvider>
                   {Object.entries(timeInDifferentUnits).map(([key, values], i) =>
                     <React.Fragment key={key + "-" + i}>
@@ -93,30 +149,49 @@ export default async function SpotifyPage() {
                   )}
                 </TooltipProvider>
               </div>
-            );
-          })}
+            )
+          })()}
+
+          {/* Absolute most listened to track */}
+          <h3 className="mt-2">Mest spelade låtar</h3>
+          <div className="flex flex-col gap-y-2 pt-2">
+            {mostListenedTracks.map(([trackId, trackMS], i) => {
+              const track = awaitedUsers.flatMap(user => user.data).find(play => play.track.id === trackId)?.track;
+              if (!track) return null;
+
+              return (
+                <TrackPlay track={track} listeningTime={parseFloat(trackMS.toString())} username={null} key={track.id + "-" + i} />
+              );
+            })}
+          </div>
+
+          {/* Absolute most listened to artist */}
         </TabsContent>
 
         {/* User tabs */}
         {users.map(async (user, i) => {
           const data = await user.data;
 
-          const totalMS = data.map(play => play.track).reduce((acc, track) => acc + track.duration, 0); // ms
+          const totalUserMS = data.map(play => play.track).reduce((acc, track) => acc + track.duration, 0); // ms
           const timeInDifferentUnits = {
-            s: { time: totalMS / 1000, unitLong: "sekunder", unitShort: "s" },
-            min: { time: totalMS / 60000, unitLong: "minuter", unitShort: "min" },
-            h: { time: totalMS / 3600000, unitLong: "timmar", unitShort: "h" },
-            d: { time: totalMS / 86400000, unitLong: "dygn", unitShort: "d" },
-            w: { time: totalMS / 604800000, unitLong: "veckor", unitShort: "v" },
-            m: { time: totalMS / 2419200000, unitLong: "månader", unitShort: "m" },
-            y: { time: totalMS / 29030400000, unitLong: "år", unitShort: "å" },
+            s: { time: totalUserMS / 1000, unitLong: "sekunder", unitShort: "s" },
+            min: { time: totalUserMS / 60000, unitLong: "minuter", unitShort: "min" },
+            h: { time: totalUserMS / 3600000, unitLong: "timmar", unitShort: "h" },
+            d: { time: totalUserMS / 86400000, unitLong: "dygn", unitShort: "d" },
+            w: { time: totalUserMS / 604800000, unitLong: "veckor", unitShort: "v" },
+            m: { time: totalUserMS / 2419200000, unitLong: "månader", unitShort: "m" },
+            y: { time: totalUserMS / 29030400000, unitLong: "år", unitShort: "å" },
           };
 
-          const tracks = data.map(play => play.track).filter((track, i, self) => self.findIndex(t => t.id === track.id) === i); // unique tracks
+          const uniqueTracks = data.map(play => play.track).filter((track, i, self) => self.findIndex(t => t.id === track.id) === i); // unique tracks
+          const listenedTimes = Object.fromEntries(uniqueTracks.map(track => {
+            const totalMS = data.filter(play => play.track.id === track.id).map(play => play.track).reduce((acc, track) => acc + track.duration, 0); // ms
+            return [track.id, totalMS];
+          }));
 
           return (
-            <TabsContent tabIndex={-1} key={user.id + "-" + i} value={encodeURIComponent(user.name || user.id)} className="w-full sm:w-8/12 flex flex-col gap-y-3">
-              {/* Total times per user */}
+            <TabsContent tabIndex={-1} key={user.id + "-" + i} value={encodeURIComponent(user.name || user.id)} className="w-full lg:w-8/12 flex flex-col gap-y-3">
+              {/* User stats */}
               <div className="flex flex-row gap-x-2 whitespace-nowrap overflow-x-scroll">
                 Total tid för {user.name}:
                 <TooltipProvider>
@@ -128,16 +203,28 @@ export default async function SpotifyPage() {
                       </Tooltip>
 
                       {/* Separator */}
-                      {i < Object.entries(timeInDifferentUnits).length - 1 && <span className="cursor-default">{"="}</span>}
+                      {/* {i < Object.entries(timeInDifferentUnits).length - 1 && <span className="cursor-default">{"="}</span>} */}
+                      <span className="cursor-default">{"="}</span>
                     </React.Fragment>
                   )}
+                  <Tooltip>
+                    <TooltipTrigger>{(totalUserMS / totalMS * 100).toFixed(2)}% av alla</TooltipTrigger>
+                    <TooltipContent>{totalUserMS / totalMS * 100}%</TooltipContent>
+                  </Tooltip>
                 </TooltipProvider>
               </div>
 
               {/* Tracks */}
-              {tracks.map((track, i) =>
-                <TrackPlay track={track} listeningTime={0} username={user.name} key={track.id + "-" + user.id + "-" + i} />
-              )}
+              {uniqueTracks
+                // Sort by listened time
+                .sort((a, b) => {
+                  const aTime = listenedTimes[a.id] || 0;
+                  const bTime = listenedTimes[b.id] || 0;
+                  return bTime - aTime;
+                })
+                .map((track, i) =>
+                  <TrackPlay track={track} listeningTime={listenedTimes[track.id]} username={user.name} key={track.id + "-" + user.id + "-" + i} />
+                )}
             </TabsContent>
           );
         })}
--- src/components/spotify/track-play.tsx
+++ src/components/spotify/track-play.tsx
@@ -1,4 +1,4 @@
-// "use cache";
+"use cache";
 
 import type { Track } from "@/types";
 import Link from "next/link";
@@ -8,20 +8,20 @@ import CrownSVG from "@root/public/icons/crown.svg" with { type: "image/svg+xml"
 import SpotifyIconSVG from "@root/public/icons/spotify/Primary_Logo_Green_RGB.svg" with { type: "image/svg+xml" };
 import { Vibrant } from "node-vibrant/node";
 
+const getPalette = async (url: string | null, quality: number) => {
+  const v = url ? new Vibrant(url, { quality: quality, useWorker: true }) : null
+  return await v?.getPalette();
+}
+
 export async function TrackPlay({ track, listeningTime, username }: { track: Track, listeningTime: number, username: string | null }) {
+  "use cache";
 
   // Track duration
   const minutes = Math.floor(track.duration / 60000);
   const seconds = Math.floor((track.duration % 60000) / 1000);
   const prettyDuration = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-  const getPalette = async (quality: number) => {
-    const v = track.image ? new Vibrant(track.image, { quality: quality, useWorker: true }) : null
-    return await v?.getPalette();
-  }
-
-  const palette = await getPalette(100);
-  // const bgColor = palette?.Vibrant?.hex || "var(--color-zinc-100)"; // Not very contrasty against black text but prettier
+  const palette = await getPalette(track.image || null, 100);
   const bgColor = palette?.LightVibrant?.hex || "var(--color-zinc-100)"; // Contrasty against black text
 
   return (
@@ -33,8 +33,17 @@ export async function TrackPlay({ track, listeningTime, username }: { track: Tra
       {/* Track info */}
       <h5 className="col-start-2 col-span-2">{track.name}</h5>
       {/* Artists */}
-      <p className="col-start-2 col-span-2 font-semibold text-sm opacity-70 -mt-1">{track.artists.map(artist => artist.name).join(", ")}</p>
+      <p className="col-start-2 col-span-2 font-semibold text-sm opacity-75 -mt-1">{track.artists.map(artist => artist.name).join(", ")}</p>
 
+      {/* Stats */}
+      <div className="row-span-2 text-sm flex-1">
+        {/* Duration */}
+        <p>Längd {minutes} min {seconds} sek ({prettyDuration})</p>
+        {/* Listening time */}
+        <p>{username ?? "Personer"} har lyssnat totalt {Math.round(listeningTime / 60000)} min</p>
+        {/* Count */}
+        <p>Det motsvara {Math.round(listeningTime / track.duration)} av låten</p>
+      </div>
 
       {/* Spotify Link */}
       <Link href={track.url} className="col-start-3 row-start-4 justify-self-end self-end" target="_blank" rel="noopener noreferrer">
