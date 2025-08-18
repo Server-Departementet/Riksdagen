d7db180902bc27379f4b3f73577a84d7265c8d46
Vena <strom.vena+gh@gmail.com>
2025-08-18 21:59:34 +0200

    update pull script

--- deploy-code/pull.sh
+++ deploy-code/pull.sh
@@ -3,14 +3,11 @@
 git fetch --all
 git checkout origin/main --force
 
-# Build
-yarn install --frozen-lockfile
-yarn prisma generate
-yarn build
-bash /root/Riksdagen/deploy-code/deploy.sh
-
 # Make /deploy-code executable
 chmod +x deploy-code/*
+
+# Build
+bash /root/Riksdagen/deploy-code/deploy.sh
 
 # Restart
 reboot
