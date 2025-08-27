16f4bf04b0f8a88e5d568b2ab28078e6a47c17aa
Vena <strom.vena+gh@gmail.com>
2025-08-27 23:11:35 +0200

    extract getters to helpers

---
+++ src/app/spotify/functions/get-albums.ts
@@ -0,0 +1,23 @@
+import { prisma } from "@/lib/prisma";
+import { Album } from "../types";
+
+export default async function getAlbums() {
+  "use cache";
+
+  const albums: Album[] = (await prisma.album.findMany({
+    include: {
+      _count: {
+        select: { tracks: true }
+      },
+    }
+  }))
+    .map(album => ({
+      id: album.id,
+      name: album.name,
+      image: album.image,
+      url: album.url,
+      trackCount: album._count.tracks,
+    }));
+
+  return albums;
+}
--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -1,54 +1,21 @@
-import type { Album, User } from "@/app/spotify/types";
 import { isMinister } from "@/lib/auth";
 import { notFound } from "next/navigation";
-import { clerkClient } from "@clerk/nextjs/server";
 import FetchFilterContextProvider from "@/app/spotify/context/fetch-filter-context";
 import LocalFilterContextProvider from "@/app/spotify/context/local-filter-context";
 import FilterPanel from "@/app/spotify/components/filter-panel";
 import TrackList from "@/app/spotify/components/track-list";
-import { prisma } from "@/lib/prisma";
+import getMinisters from "./functions/get-users";
+import getAlbums from "./functions/get-albums";
 
-const clerk = await clerkClient();
-
-async function readUsers() {
-  "use cache";
-
-  const allUsers = (await clerk.users.getUserList({ orderBy: "+created_at" })).data;
-  const users = (await Promise.all(
-    allUsers.map(async user => {
-      const isMinisterUser = await isMinister(user.id);
-      return isMinisterUser ? { id: user.id, name: user.firstName || user.id } : null;
-    })
-  )).filter(Boolean) as User[];
-
-  return users;
-}
 
 export default async function SpotifyPage() {
   // Auth check
   if (!(await isMinister())) return notFound();
 
-  const users = await readUsers();
-
-  const userMap: Record<string, User> = {};
-  users.forEach(user => {
-    userMap[user.id] = user;
-  });
-
-  const albums: Album[] = (await prisma.album.findMany({
-    include: {
-      _count: {
-        select: { tracks: true }
-      },
-    }
-  }))
-    .map(album => ({
-      id: album.id,
-      name: album.name,
-      image: album.image,
-      url: album.url,
-      trackCount: album._count.tracks,
-    }));
+  const [users, albums] = await Promise.all([
+    getMinisters(),
+    getAlbums(),
+  ])
 
   return (
     <main className={`
@@ -62,12 +29,12 @@ export default async function SpotifyPage() {
         flex flex-col sm:flex-row justify-center
         w-full
       `}>
-        <FetchFilterContextProvider initialUsers={users}>
+        <FetchFilterContextProvider initialUsers={Object.values(users)}>
           <LocalFilterContextProvider>
 
             {/* Filter panel will set fetch filters for getting track ids. Local filters such as search will also live here */}
             <FilterPanel
-              userMap={userMap}
+              userMap={users}
               albums={albums}
               className="flex-1"
             />
