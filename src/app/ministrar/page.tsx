75b19829478122c2a080e88577127f3c8ac8a0e1
Vena <strom.vena+gh@gmail.com>
2025-08-28 16:53:01 +0200

    update page meta

--- src/app/ministrar/page.tsx
+++ src/app/ministrar/page.tsx
@@ -2,7 +2,8 @@ import { MinistersList } from "@/components/minister-posts";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Ministrar"
+  title: "Ministrar",
+  description: "Lista över ministrarna i 'Regeringen'.",
 };
 
 export default function Page() {
