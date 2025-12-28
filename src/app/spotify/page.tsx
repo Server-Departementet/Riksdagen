6d495708116c7b41da1dacacf4183909a7e0186e
VenaStrom <strom.vena+gh@gmail.com>
2025-12-28 23:00:49 +0100

    cleanup

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -10,77 +10,14 @@ import FilterPanel from "@/app/spotify/components/filter-panel";
 import TrackList from "@/app/spotify/components/track-list";
 import { prisma } from "@/lib/prisma";
 
-const clerk = await clerkClient();
-
-async function readUsers() {
-  "use cache";
-
-  const allUsers = (await clerk.users.getUserList({ orderBy: "+created_at" })).data;
-  const users = (await Promise.all(
-    allUsers.map(async user => {
-      const isMinisterUser = await isMinister(user.id);
-      return isMinisterUser ? { id: user.id, name: user.firstName || user.id } : null;
-    })
-  )).filter(Boolean) as User[];
-
-  return users;
-}
+  const clerk = await clerkClient();
 
 export default async function SpotifyPage() {
   const userId = (await auth()).userId;
   if (!userId) return notFound();
   if (!await isMinister(userId)) return notFound();
 
-  const users = await readUsers();
+  return <main>
 
-  const userMap: Record<string, User> = {};
-  users.forEach(user => {
-    userMap[user.id] = user;
-  });
-
-  const albums: Album[] = (await prisma.album.findMany({
-    include: {
-      _count: {
-        select: { tracks: true }
-      },
-    }
-  }))
-    .map(album => ({
-      id: album.id,
-      name: album.name,
-      image: album.image,
-      url: album.url,
-      trackCount: album._count.tracks,
-    }));
-
-  return (
-    <main className={`
-      px-0 lg:px-4
-      sm:max-h-(--screen-height) sm:min-h-(--screen-height) sm:h-(--screen-height) sm:*:min-h-[inherit] sm:*:h-[inherit]
-    `}>
-      {/* Overwrite layout styling */}
-      <style>{`footer{display:none;}main{padding:0;}`}</style>
-
-      <section className={`
-        flex flex-col sm:flex-row justify-center
-        w-full
-      `}>
-        <FetchFilterContextProvider initialUsers={users}>
-          <LocalFilterContextProvider>
-
-            {/* Filter panel will set fetch filters for getting track ids. Local filters such as search will also live here */}
-            <FilterPanel
-              userMap={userMap}
-              albums={albums}
-              className="flex-1"
-            />
-
-            {/* Filtered tracks */}
-            <TrackList className="flex-2" />
-
-          </LocalFilterContextProvider>
-        </FetchFilterContextProvider>
-      </section>
-    </main>
-  );
+  </main>;
 }
