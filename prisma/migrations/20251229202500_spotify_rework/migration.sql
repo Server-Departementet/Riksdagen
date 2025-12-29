380808d1cd5e57ae14aae52186f6bd242ef75030
VenaStrom <strom.vena+gh@gmail.com>
2025-12-29 22:06:08 +0100

    update latest migration

--- prisma/migrations/20251229202500_spotify_rework/migration.sql
+++ prisma/migrations/20251229202500_spotify_rework/migration.sql
@@ -22,14 +22,27 @@ ALTER TABLE `TrackPlay` DROP PRIMARY KEY,
 -- CreateTable
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `clerkDevId` VARCHAR(191) NOT NULL,
-    `clerkProdId` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `clerkDevId` VARCHAR(191) NULL,
+    `clerkProdId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_clerkDevId_key`(`clerkDevId`),
     UNIQUE INDEX `User_clerkProdId_key`(`clerkProdId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Alter user table to make name and clerk ids optional
+-- ALTER TABLE `User`
+--     MODIFY COLUMN `name` VARCHAR(191) NULL,
+--     MODIFY COLUMN `clerkDevId` VARCHAR(191) NULL,
+--     MODIFY COLUMN `clerkProdId` VARCHAR(191) NULL;
+
+-- Create a user for each userId
+INSERT INTO `User` (`id`)
+SELECT DISTINCT `userId` FROM `TrackPlay` tp
+WHERE NOT EXISTS (
+    SELECT 1 FROM `User` u WHERE u.`id` = tp.`userId`
+);
 
 -- AddForeignKey
 ALTER TABLE `TrackPlay` ADD CONSTRAINT `TrackPlay_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
