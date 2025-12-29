5003f8c6dfdef4cc304d8921f12d5932a15b947f
VenaStrom <strom.vena+gh@gmail.com>
2025-12-29 23:18:20 +0100

    WIP rewrite spotify endpoint

--- src/app/api/spotify/post/route.ts
+++ src/app/api/spotify/post/route.ts
@@ -7,12 +7,185 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { createHash } from "node:crypto";
+import { Prisma } from "@/prisma/client";
 
-function generateDeterministicId(userId: string, track: SpotifyApi.TrackObjectFull, playedAt: string): string {
-  const uniqueString = `${userId}:${track.id}:${playedAt}`;
-  return createHash("md5").update(uniqueString).digest("hex");
+const lastFetch: Date = new Date();
+const fetchInterval = 60 * 1000; // 1 minute
+
+export async function POST() {
+  if (lastFetch && lastFetch > new Date(Date.now() - fetchInterval)) {
+    console.warn("API already fetched recently. Skipping this run.");
+    return NextResponse.json({ message: "Too Many Requests", }, { status: 429 });
+  }
+  lastFetch.setTime(Date.now());
+
+  const client = await clerkClient();
+  const users = await client.users.getUserList();
+
+  const ministers = users.data.filter((user) => user.publicMetadata?.role === "minister");
+
+  for (const clerkUser of ministers) {
+    /* 
+     * Get spotify OAuth token for the user 
+     */
+    const tokenResponse = await client.users.getUserOauthAccessToken(clerkUser.id, "spotify");
+    if (!tokenResponse.data.length) {
+      console.warn(`No Spotify token found for user: ${clerkUser.firstName}`);
+      continue;
+    }
+    if (tokenResponse.data.length > 1) {
+      console.warn(`Multiple Spotify tokens found for user: ${clerkUser.firstName}. Only using the first one.`);
+    }
+    const spotifyToken = tokenResponse.data[0].token;
+
+    /* 
+     * Ensure user is in our database 
+     */
+    const dbUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { clerkDevId: clerkUser.id, },
+          { clerkProdId: clerkUser.id, },
+          // It should only be one of the above but in migration clerk ids may have been dumped as User.id  TODO remove
+          { id: clerkUser.id, },
+        ],
+      },
+    });
+    if (!dbUser) {
+      console.warn(`User ${clerkUser.firstName} not found in database. Skipping.`);
+      continue;
+    }
+
+    /* 
+     * Get recently played tracks from Spotify API 
+     */
+    const recentlyPlayedTracksResponse = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${spotifyToken}`,
+      },
+    });
+    if (!recentlyPlayedTracksResponse.ok) {
+      console.error(`Error for user ${clerkUser.firstName}: Status ${recentlyPlayedTracksResponse.status} Response: ${await recentlyPlayedTracksResponse.text()}`);
+      continue;
+    }
+    const recentlyPlayedTracks: SpotifyApi.UsersRecentlyPlayedTracksResponse = await recentlyPlayedTracksResponse.json();
+    if (!recentlyPlayedTracks.items || recentlyPlayedTracks.items.length === 0) {
+      console.warn(`No recently played tracks found for user: ${clerkUser.firstName}`);
+      continue;
+    }
+
+    /* 
+     * Ensure all tracks, albums, artists, genres are in our database 
+     */
+    // for (const trackPlay of recentlyPlayedTracks.items) {
+    //   const missingTracks = await prisma.track.findUnique({
+    // }
+    const missingTrackIds = await prisma.track.findMany({
+      where: {
+        id: { in: recentlyPlayedTracks.items.map((item) => item.track.id) },
+      },
+      select: { id: true },
+    });
+    const missingAlbumIds = await prisma.album.findMany({
+      where: {
+        id: { in: recentlyPlayedTracks.items.map((item) => item.track.album.id) },
+      },
+      select: { id: true },
+    });
+    const missingArtistIds = await prisma.artist.findMany({
+      where: {
+        id: { in: recentlyPlayedTracks.items.flatMap((item) => item.track.artists.map((artist) => artist.id)) },
+      },
+      select: { id: true },
+    });
+    /** 
+     * FETCH ARTISTS!
+     */
+
+    // const missingGenreNames = await prisma.genre.findMany({
+    //   where: {
+    //     name: { in: recentlyPlayedTracks.items.flatMap((item) => item.track.artists).flatMap((artist) => artist.genres || []) },
+    //   },
+    //   select: { name: true },
+    // });
+    // const addedArtists = await prisma.artist.createMany({
+    //   data: recentlyPlayedTracks.items
+    //     .flatMap((item) => item.track.artists)
+    //     .filter((artist) => !missingArtistIds.find((a) => a.id === artist.id))
+    //     .map((artist) => ({
+    //       id: artist.id,
+    //       name: artist.name,
+    //       url: artist.external_urls.spotify,
+    //     } satisfies Prisma.ArtistCreateManyInput)),
+    //   skipDuplicates: true,
+    // });
+    const addedTracks = await prisma.track.createMany({
+      data: recentlyPlayedTracks.items
+        .filter((item) => !missingTrackIds.find((t) => t.id === item.track.id))
+        .map((item) => ({
+          id: item.track.id,
+          name: item.track.name,
+          duration: item.track.duration_ms,
+          url: item.track.external_urls.spotify,
+          albumId: item.track.album.id,
+        } satisfies Prisma.TrackCreateManyInput)),
+      skipDuplicates: true,
+    });
+
+
+    /* 
+     * Process and store each fetched track play
+     */
+    const trackPlays = await prisma.trackPlay.createMany({
+      data: recentlyPlayedTracks.items.map((item) => ({
+        userId: dbUser.id,
+        trackId: item.track.id,
+        playedAt: new Date(item.played_at),
+      } satisfies Prisma.TrackPlayCreateManyInput)),
+      skipDuplicates: true,
+    });
+
+    return NextResponse.json({ message: "200 OK" }, { status: 200 });
+  }
 }
+
+
+// if (data.items && data.items.length > 0) {
+//   const trackPlaysDB: Set<string> = new Set(
+//     (
+//       await prisma.trackPlay.findMany({
+//         select: { id: true },
+//       })
+//     ).map((play) => play.id)
+//   );
+
+//   data.items = data.items.filter((item) => !item.track.is_local);
+
+//   data.items = data.items.map((item: SpotifyApi.PlayHistoryObject) => {
+//     return {
+//       ...item,
+//       track: {
+//         ...item.track,
+//         generatedId: generateDeterministicId(user.id, item.track, item.played_at),
+//       },
+//     }
+//   })
+//     .filter((item) => !trackPlaysDB.has(item.track.generatedId));
+
+//   for (const item of data.items) {
+//     try {
+//       const track = item.track;
+//       const playedAt = item.played_at;
+
+//       await storeTrackPlay(user.id, track, playedAt, token);
+//     } catch (error) {
+//       console.error(`Error storing track play for ${user.firstName}:`, error);
+//     }
+//   }
+// } else {
+//   console.warn("No recently played tracks found.");
+// }
 
 async function storeTrackPlay(userId: string, track: SpotifyApi.TrackObjectFull, playedAt: string, token: string) {
   // Generate a deterministic ID using userId, track.id, and playedAt timestamp.
@@ -132,97 +305,5 @@ async function storeTrackPlay(userId: string, track: SpotifyApi.TrackObjectFull,
       userId: userId,
       playedAt: new Date(playedAt),
     },
-  });
-}
-
-const lastFetch: Date = new Date();
-const fetchInterval = 60 * 1000; // 1 minute
-
-export async function POST() {
-  if (lastFetch && lastFetch > new Date(Date.now() - fetchInterval)) {
-    console.warn("API already fetched recently. Skipping this run.");
-    return NextResponse.json({
-      message: "Already fetched recently. Skipping.",
-    });
-  }
-  lastFetch.setTime(Date.now());
-
-  const client = await clerkClient();
-  const users = await client.users.getUserList();
-
-  const ministers = users.data.filter((user) => user.publicMetadata?.role === "minister");
-
-  for (const user of ministers) {
-    try {
-      const tokenResponse = await client.users.getUserOauthAccessToken(user.id, "spotify");
-
-      if (!tokenResponse.data.length) {
-        console.warn(`No Spotify token found for user: ${user.firstName}`);
-        continue;
-      }
-
-      if (tokenResponse.data.length > 1) {
-        console.warn(`Multiple Spotify tokens found for user: ${user.firstName}. Only using the first one.`);
-      }
-
-      const token = tokenResponse.data[0].token;
-
-      const response = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        console.error(`Error for user ${user.firstName}: Status ${response.status}`);
-        console.error(`Response: ${await response.text()}`);
-        continue;
-      }
-
-      const data: SpotifyApi.UsersRecentlyPlayedTracksResponse = await response.json();
-
-      if (data.items && data.items.length > 0) {
-        const trackPlaysDB: Set<string> = new Set(
-          (
-            await prisma.trackPlay.findMany({
-              select: { id: true },
-            })
-          ).map((play) => play.id)
-        );
-
-        data.items = data.items.filter((item) => !item.track.is_local);
-
-        data.items = data.items.map((item: SpotifyApi.PlayHistoryObject) => {
-          return {
-            ...item,
-            track: {
-              ...item.track,
-              generatedId: generateDeterministicId(user.id, item.track, item.played_at),
-            },
-          }
-        })
-          .filter((item) => !trackPlaysDB.has(item.track.generatedId));
-
-        for (const item of data.items) {
-          try {
-            const track = item.track;
-            const playedAt = item.played_at;
-
-            await storeTrackPlay(user.id, track, playedAt, token);
-          } catch (error) {
-            console.error(`Error storing track play for ${user.firstName}:`, error);
-          }
-        }
-      } else {
-        console.warn("No recently played tracks found.");
-      }
-    } catch (error) {
-      console.error(`Error processing user ${user.firstName}:`, error);
-    }
-  }
-
-  return NextResponse.json({
-    message: "200 OK",
   });
 }
