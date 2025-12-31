82770367bff91abbe275eae1cf636feb8ddd2c71
VenaStrom <strom.vena+gh@gmail.com>
2025-12-31 13:13:03 +0100

    update spotify post route

--- src/app/api/spotify/post/route.ts
+++ src/app/api/spotify/post/route.ts
@@ -8,6 +8,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { Prisma } from "@/prisma/client";
+import { getTrackBGColor } from "@/app/spotify/functions/get-track-color";
 
 const lastFetch: Date = new Date();
 const fetchInterval = 60 * 1000; // 1 minute
@@ -59,169 +60,158 @@ export async function POST() {
     /* 
      * Get recently played tracks from Spotify API 
      */
-    const recentlyPlayedTracksResponse = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${spotifyToken}`,
-      },
-    });
-    if (!recentlyPlayedTracksResponse.ok) {
-      console.error(`Error for user ${clerkUser.firstName}: Status ${recentlyPlayedTracksResponse.status} Response: ${await recentlyPlayedTracksResponse.text()}`);
-      continue;
-    }
-    const recentlyPlayedTracks: SpotifyApi.UsersRecentlyPlayedTracksResponse = await recentlyPlayedTracksResponse.json();
-    if (!recentlyPlayedTracks.items || recentlyPlayedTracks.items.length === 0) {
-      console.warn(`No recently played tracks found for user: ${clerkUser.firstName}`);
+    const recentlyPlayedTracks = await getRecentlyPlayedTracks(spotifyToken, clerkUser.firstName || "[Unknown user]");
+    if (!recentlyPlayedTracks) {
       continue;
     }
 
     /* 
-     * Ensure all tracks, albums, artists, genres are in our database 
+     * Prepare data for upserting to database
      */
-    // for (const trackPlay of recentlyPlayedTracks.items) {
-    //   const missingTracks = await prisma.track.findUnique({
-    // }
-    const missingTrackIds = await prisma.track.findMany({
-      where: {
-        id: { in: recentlyPlayedTracks.items.map((item) => item.track.id) },
-      },
-      select: { id: true },
-    });
-    const missingAlbumIds = await prisma.album.findMany({
-      where: {
-        id: { in: recentlyPlayedTracks.items.map((item) => item.track.album.id) },
-      },
-      select: { id: true },
-    });
-    const missingArtistIds = await prisma.artist.findMany({
-      where: {
-        id: { in: recentlyPlayedTracks.items.flatMap((item) => item.track.artists.map((artist) => artist.id)) },
-      },
-      select: { id: true },
-    });
-    /** 
-     * FETCH ARTISTS!
-     */
+    const existingArtistIds = await prisma.artist.findMany({ select: { id: true }, });
+    const missingArtistsSimple = recentlyPlayedTracks.items
+      .flatMap((item) => item.track.artists)
+      .filter((artist) => !existingArtistIds.find((a) => a.id === artist.id));
+    const artists = await getSpotifyArtists(missingArtistsSimple, spotifyToken);
+    const genres = artists.flatMap((artist) => artist.genres);
+    const albums = recentlyPlayedTracks.items.map((item) => item.track.album);
+    const tracks = recentlyPlayedTracks.items.map((item) => item.track);
 
-    // const missingGenreNames = await prisma.genre.findMany({
-    //   where: {
-    //     name: { in: recentlyPlayedTracks.items.flatMap((item) => item.track.artists).flatMap((artist) => artist.genres || []) },
-    //   },
-    //   select: { name: true },
-    // });
-    // const addedArtists = await prisma.artist.createMany({
-    //   data: recentlyPlayedTracks.items
-    //     .flatMap((item) => item.track.artists)
-    //     .filter((artist) => !missingArtistIds.find((a) => a.id === artist.id))
-    //     .map((artist) => ({
-    //       id: artist.id,
-    //       name: artist.name,
-    //       url: artist.external_urls.spotify,
-    //     } satisfies Prisma.ArtistCreateManyInput)),
-    //   skipDuplicates: true,
-    // });
-    const addedTracks = await prisma.track.createMany({
-      data: recentlyPlayedTracks.items
-        .filter((item) => !missingTrackIds.find((t) => t.id === item.track.id))
-        .map((item) => ({
-          id: item.track.id,
-          name: item.track.name,
-          duration: item.track.duration_ms,
-          url: item.track.external_urls.spotify,
-          albumId: item.track.album.id,
-        } satisfies Prisma.TrackCreateManyInput)),
-      skipDuplicates: true,
-    });
-
+    // Colors
+    const colors: Record<string, string> = {};
+    const allImageUrls = [
+      ...artists.map((artist) => artist.images[0]?.url).filter((url): url is string => !!url),
+      ...albums.map((album) => album.images[0]?.url).filter((url): url is string => !!url),
+    ];
+    await Promise.all(allImageUrls.map(async (url) => {
+      if (colors[url]) return;
+      const color = await getTrackBGColor(url);
+      if (!color) return;
+      colors[url] = color;
+    }));
 
     /* 
-     * Process and store each fetched track play
+     * Write Genres, Artists, Albums, Tracks and TrackPlays to database
      */
-    const trackPlays = await prisma.trackPlay.createMany({
-      data: recentlyPlayedTracks.items.map((item) => ({
-        userId: dbUser.id,
-        trackId: item.track.id,
-        playedAt: new Date(item.played_at),
-      } satisfies Prisma.TrackPlayCreateManyInput)),
-      skipDuplicates: true,
-    });
+    await prisma.$transaction(async (prisma) => {
+      // Insert Genres, skip dupes
+      await prisma.genre.createMany({
+        skipDuplicates: true,
+        data: [
+          ...genres.map((genre) => ({ name: genre }))
+        ] satisfies Prisma.GenreCreateManyInput[],
+      });
 
-    return NextResponse.json({ message: "200 OK" }, { status: 200 });
+      // Upsert Albums
+      for (const album of albums) {
+        const imageUrl = album.images[0]?.url || null;
+        await prisma.album.upsert({
+          where: { id: album.id },
+          update: {
+            name: album.name,
+            url: album.external_urls.spotify,
+            image: imageUrl,
+            color: imageUrl ? colors[imageUrl] : undefined,
+            tracks: {
+              connect: tracks
+                .filter((track) => track.album.id === album.id)
+                .map((track) => ({ id: track.id })),
+            },
+          },
+          create: {
+            id: album.id,
+            name: album.name,
+            url: album.external_urls.spotify,
+            image: imageUrl,
+            color: imageUrl ? colors[imageUrl] : undefined,
+            tracks: {
+              connect: tracks
+                .filter((track) => track.album.id === album.id)
+                .map((track) => ({ id: track.id })),
+            },
+          },
+        });
+      }
+
+      // Upsert tracks
+      for (const track of tracks) {
+        await prisma.track.upsert({
+          where: { id: track.id },
+          update: {
+            name: track.name,
+            url: track.external_urls.spotify,
+            duration: track.duration_ms,
+            albumId: track.album.id,
+          },
+          create: {
+            id: track.id,
+            name: track.name,
+            url: track.external_urls.spotify,
+            duration: track.duration_ms,
+            albumId: track.album.id,
+          },
+        });
+      }
+
+      // Upsert Artists
+      for (const artist of artists) {
+        const imageUrl = artist.images[0]?.url || null;
+        await prisma.artist.upsert({
+          where: { id: artist.id },
+          update: {
+            name: artist.name,
+            url: artist.external_urls.spotify,
+            image: imageUrl,
+            color: imageUrl ? colors[imageUrl] : undefined,
+            genres: {
+              connect: artist.genres.map((genre) => ({ name: genre })),
+            },
+            tracks: {
+              connect: tracks
+                .filter((track) => track.artists.some((a) => a.id === artist.id))
+                .map((track) => ({ id: track.id })),
+            },
+          },
+          create: {
+            id: artist.id,
+            name: artist.name,
+            url: artist.external_urls.spotify,
+            image: imageUrl,
+            color: imageUrl ? colors[imageUrl] : undefined,
+            genres: {
+              connect: artist.genres.map((genre) => ({ name: genre })),
+            },
+            tracks: {
+              connect: tracks
+                .filter((track) => track.artists.some((a) => a.id === artist.id))
+                .map((track) => ({ id: track.id })),
+            },
+          },
+        });
+      }
+
+      // Insert TrackPlays, skip dupes
+      await prisma.trackPlay.createMany({
+        skipDuplicates: true,
+        data: recentlyPlayedTracks.items.map((item) => ({
+          playedAt: new Date(item.played_at),
+          userId: dbUser.id,
+          trackId: item.track.id,
+        })) satisfies Prisma.TrackPlayCreateManyInput[],
+      });
+    })
+      .catch((error) => {
+        console.error(`Error upserting data for user ${clerkUser.firstName}:`, error);
+      });
   }
+
+  return NextResponse.json({ message: "200 OK" }, { status: 200 });
 }
 
+async function getSpotifyArtists(artistsSimple: SpotifyApi.ArtistObjectSimplified[], token: string): Promise<SpotifyApi.ArtistObjectFull[]> {
+  const artistDetails: SpotifyApi.ArtistObjectFull[] = [];
 
-// if (data.items && data.items.length > 0) {
-//   const trackPlaysDB: Set<string> = new Set(
-//     (
-//       await prisma.trackPlay.findMany({
-//         select: { id: true },
-//       })
-//     ).map((play) => play.id)
-//   );
-
-//   data.items = data.items.filter((item) => !item.track.is_local);
-
-//   data.items = data.items.map((item: SpotifyApi.PlayHistoryObject) => {
-//     return {
-//       ...item,
-//       track: {
-//         ...item.track,
-//         generatedId: generateDeterministicId(user.id, item.track, item.played_at),
-//       },
-//     }
-//   })
-//     .filter((item) => !trackPlaysDB.has(item.track.generatedId));
-
-//   for (const item of data.items) {
-//     try {
-//       const track = item.track;
-//       const playedAt = item.played_at;
-
-//       await storeTrackPlay(user.id, track, playedAt, token);
-//     } catch (error) {
-//       console.error(`Error storing track play for ${user.firstName}:`, error);
-//     }
-//   }
-// } else {
-//   console.warn("No recently played tracks found.");
-// }
-
-async function storeTrackPlay(userId: string, track: SpotifyApi.TrackObjectFull, playedAt: string, token: string) {
-  // Generate a deterministic ID using userId, track.id, and playedAt timestamp.
-  const deterministicId = generateDeterministicId(userId, track, playedAt);
-
-  // Upsert the Album for the track.
-  await prisma.album.upsert({
-    where: { id: track.album.id },
-    update: {
-      name: track.album.name,
-      url: track.album.external_urls.spotify,
-      image: track.album.images[0]?.url || null,
-    },
-    create: {
-      id: track.album.id,
-      name: track.album.name,
-      url: track.album.external_urls.spotify,
-      image: track.album.images[0]?.url || null,
-    },
-  });
-
-  // Check database for existing artists.
-  const existingArtists = new Set(
-    (
-      await prisma.artist.findMany({
-        where: {
-          id: { in: track.artists.map((artist: SpotifyApi.ArtistObjectSimplified) => artist.id) },
-        },
-        select: { id: true },
-      })
-    ).map((artist) => artist.id)
-  );
-
-  // Upsert each Artist and handle Genre connections.
-  for (const artist of track.artists.filter((artist) => !existingArtists.has(artist.id))) {
-    // Fetch artist details from Spotify API.
+  for (const artist of artistsSimple) {
     const response = await fetch(artist.href, {
       method: "GET",
       headers: {
@@ -236,74 +226,30 @@ async function storeTrackPlay(userId: string, track: SpotifyApi.TrackObjectFull,
     }
 
     const data: SpotifyApi.SingleArtistResponse = await response.json();
-
-    const genres: string[] = data.genres;
-
-    const genreConnect = genres.map((genre: string) => ({ name: genre }));
-
-    // Upsert Genre entries for the artist.
-    for (const genre of genres) {
-      await prisma.genre.upsert({
-        where: { name: genre },
-        update: {},
-        create: { name: genre },
-      });
-    }
-
-    await prisma.artist.upsert({
-      where: { id: artist.id },
-      update: {
-        name: artist.name,
-        url: artist.external_urls.spotify,
-        image: data.images[0]?.url || null,
-        genres: { set: genreConnect },
-      },
-      create: {
-        id: artist.id,
-        name: artist.name,
-        url: artist.external_urls.spotify,
-        image: data.images[0]?.url || null,
-        genres: { connect: genreConnect },
-      },
-    });
+    artistDetails.push(data);
   }
 
-  // Upsert Track, and connect the artists that have been upserted.
-  await prisma.track.upsert({
-    where: { id: track.id },
-    update: {
-      name: track.name,
-      duration: track.duration_ms,
-      url: track.external_urls.spotify,
-      image: track.album.images[0]?.url || null,
-      albumId: track.album.id,
-      // Connect artists using their ids.
-      artists: {
-        set: track.artists.map((artist: SpotifyApi.ArtistObjectSimplified) => ({ id: artist.id })),
-      },
-    },
-    create: {
-      id: track.id,
-      name: track.name,
-      duration: track.duration_ms,
-      url: track.external_urls.spotify,
-      image: track.album.images[0]?.url || null,
-      albumId: track.album.id,
-      artists: {
-        connect: track.artists.map((artist: SpotifyApi.ArtistObjectSimplified) => ({ id: artist.id })),
-      },
-    },
-  });
+  return artistDetails;
+}
 
-  // Finally, upsert the TrackPlay entry referencing the track.
-  await prisma.trackPlay.upsert({
-    where: { id: deterministicId },
-    update: {},
-    create: {
-      id: deterministicId,
-      trackId: track.id,
-      userId: userId,
-      playedAt: new Date(playedAt),
+async function getRecentlyPlayedTracks(token: string, username: string): Promise<SpotifyApi.UsersRecentlyPlayedTracksResponse | null> {
+  const recentlyPlayedTracksResponse = await fetch("https://api.spotify.com/v1/me/player/recently-played?limit=50", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
   });
+  if (!recentlyPlayedTracksResponse.ok) {
+    console.error(`Error for user ${username}: Status ${recentlyPlayedTracksResponse.status} Response: ${await recentlyPlayedTracksResponse.text()}`);
+    return null;
+  }
+  const recentlyPlayedTracks: SpotifyApi.UsersRecentlyPlayedTracksResponse = await recentlyPlayedTracksResponse.json();
+  if (!recentlyPlayedTracks.items || recentlyPlayedTracks.items.length === 0) {
+    console.warn(`No recently played tracks found for user: ${username}`);
+    return null;
+  }
+  // Filter out local tracks
+  recentlyPlayedTracks.items = recentlyPlayedTracks.items.filter((item) => !item.track.is_local);
+
+  return recentlyPlayedTracks;
 }
