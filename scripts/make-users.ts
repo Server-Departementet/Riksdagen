464e69e4c3f251c372cdf48c9ae9d68c74883df7
VenaStrom <strom.vena+gh@gmail.com>
2026-01-01 13:33:51 +0100

    do user migration in make users script

--- scripts/make-users.ts
+++ scripts/make-users.ts
@@ -1,7 +1,7 @@
 import { idGroups } from "./complete-usermap.ts";
 import "dotenv/config";
 import { env } from "node:process";
-import { PrismaClient } from "../src/prisma/generated/client.js";
+import { PrismaClient, User } from "../src/prisma/generated/client.js";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { Client as DiscordClient, GatewayIntentBits } from "discord.js";
 
@@ -41,40 +41,51 @@ async function makeUsers() {
   });
   const prisma = new PrismaClient({ adapter });
 
+  /* 
+   * Get users nicknames on Discord via bot (or local file if available).
+   */
   const serverNicks: Record<string, string> = {};
+  try {
+    const loadedFromFile = (await import("./server-nicks.ts")).serverNicks;
 
-  const discordClient = new DiscordClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
-  await discordClient.login(env.DISCORD_BOT_TOKEN);
-  async function onDiscordReady(client: DiscordClient) {
-    if (!client.user) {
-      throw new Error("Discord client user is not defined after ready");
-    }
-    console.info(`Logged in as ${client.user.tag}`);
-
-    const guild = await client.guilds.fetch(env.REGERINGEN_GUILD_ID!);
-    const members = await guild.members.fetch();
-
-    for (const discordId of idGroups.map(g => g.discordId)) {
-      const member = members.find(m => m.user.id === discordId);
-      if (member) {
-        serverNicks[discordId] = member.nickname ?? member.user.globalName ?? member.user.username;
-      }
-    }
-
-    await client.destroy()
-      .catch((error) => {
-        console.error("Error destroying Discord client:", error);
-      });
+    Object.entries(loadedFromFile).forEach(([discordId, nick]) => {
+      serverNicks[discordId] = nick;
+    });
   }
-  await new Promise<void>((resolve, reject) => {
-    discordClient.once("ready", () => {
-      onDiscordReady(discordClient).then(resolve).catch((e) => {
-        const error = e instanceof Error ? e : new Error(String(e));
-        console.error("Error in onDiscordReady:", error);
-        reject(error);
+  catch {
+    const discordClient = new DiscordClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+    await discordClient.login(env.DISCORD_BOT_TOKEN);
+    async function onDiscordReady(client: DiscordClient) {
+      if (!client.user) {
+        throw new Error("Discord client user is not defined after ready");
+      }
+      console.info(`Logged in as ${client.user.tag}`);
+
+      const guild = await client.guilds.fetch(env.REGERINGEN_GUILD_ID!);
+      const members = await guild.members.fetch();
+
+      for (const discordId of idGroups.map(g => g.discordId)) {
+        const member = members.find(m => m.user.id === discordId);
+        if (member) {
+          serverNicks[discordId] = member.nickname ?? member.user.globalName ?? member.user.username;
+        }
+      }
+
+      await client.destroy()
+        .catch((error) => {
+          console.error("Error destroying Discord client:", error);
+        });
+    }
+    await new Promise<void>((resolve, reject) => {
+      discordClient.once("clientReady", () => {
+        onDiscordReady(discordClient).then(resolve).catch((e) => {
+          const error = e instanceof Error ? e : new Error(String(e));
+          console.error("Error in onDiscordReady:", error);
+          reject(error);
+        });
       });
     });
-  });
+  }
 
   for (const idGroup of idGroups) {
     await prisma.user.upsert({
@@ -95,4 +106,30 @@ async function makeUsers() {
       },
     });
   }
+
+  // Now the proper users are made but fk constraints may still point to old clerk ids.
+  const allUsers = await prisma.user.findMany();
+  const oldUsers = allUsers.filter(u => !u.name && !u.clerkDevId && !u.clerkProdId);
+  const userMigrationMap: Record<string, User> = {};
+  for (const oldUser of oldUsers) {
+    const matchingNewUser = allUsers.find(u => u.clerkDevId === oldUser.id || u.clerkProdId === oldUser.id);
+    userMigrationMap[oldUser.id] = matchingNewUser!;
+  }
+  for (const [oldUserId, newUser] of Object.entries(userMigrationMap)) {
+    await prisma.trackPlay.updateMany({
+      where: {
+        userId: oldUserId,
+      },
+      data: {
+        userId: newUser.id,
+      },
+    });
+  }
+
+  // Delete old users (should throw if fk constraints are still present).
+  await prisma.user.deleteMany({
+    where: {
+      id: { in: Object.keys(userMigrationMap) },
+    },
+  });
 }
