3ad2e610659c8d5857a1b79ba87b7e1830f07af6
VenaStrom <strom.vena+gh@gmail.com>
2026-02-22 17:07:23 +0100

    fix multiline in raw quotes

--- src/app/citat/page.tsx
+++ src/app/citat/page.tsx
@@ -1,15 +1,44 @@
 import { Quote } from "@/app/citat/types";
+import { ExternalLinkIcon } from "lucide-react";
 import fs from "node:fs";
+
+function isMultiSpeakerQuote(content: string): boolean {
+  const isMultiLine =
+    content.includes("\n")
+    && content.split("\n").every(line => line.trim().startsWith("\"") && line.trim().includes("-"));
+  return isMultiLine;
+}
 
 export default function QuoteStatsPage() {
   const quotes: Quote[] = JSON.parse(fs.readFileSync("scripts/discord/quotes.json", "utf-8"));
 
   return <main>
     <h1 className="mt-4">Citatstatistik</h1>
+
     <ul>
       {quotes.map(q => (
-        <li key={q.id}>
-          {q.body} - {q.quotee}
+        <li
+          key={q.id}
+          className="*:ms-3 not-first:border-t-2 py-1 flex flex-row "
+        >
+          <a
+            href={q.link}
+            className="global min-w-fit"
+            target="_blank"
+            rel="noreferrer"
+          >
+            discord&nbsp;
+            <ExternalLinkIcon className="size-3 inline mb-0.5" />
+          </a>
+          <span className="min-w-fit">{new Date(q.createdTimestamp).toLocaleString()}</span>
+          {isMultiSpeakerQuote(q.body)
+            ? <p className="whitespace-pre-wrap">
+              {q.body}
+            </p>
+            : <span>
+              {q.body} - {q.quotee}
+            </span>
+          }
         </li>
       ))}
     </ul>
