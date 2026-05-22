6fdc7f04fe41994247ae1a1564f35908d514791e
VenaStrom <strom.vena+gh@gmail.com>
2026-05-22 18:22:51 +0200

    add public to allowed routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -31,6 +31,9 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
 
+    // Allow public
+    "/(public|trpc)(.*)",
+
     // Always run for API routes
     "/(api|trpc)(.*)",
 
